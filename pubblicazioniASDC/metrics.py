f94091a17dcf5e1487474e7d08e4f37d443bebc8
Federica <federica.moscato@gmail.com>
2015-05-24 23:15:05 +0200

    removed connection string  details
    removed connection string  details

--- pubblicazioniASDC/metrics.py
+++ pubblicazioniASDC/metrics.py
@@ -178,7 +178,7 @@ def present_ASDC_overview():
 
 
 
-connection_string = "mongodb://localhost"
+connection_string = ""
 connection = pymongo.MongoClient(connection_string)
 database = connection.publication
 
